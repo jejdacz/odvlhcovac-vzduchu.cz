0115c57ce18c528e17ef1ee44fa218997e9dc9db
jejdacz <markhunter@seznam.cz>
2019-03-03 00:05:25 +0100

    bg hero fix

--- dist/odvlhcovac-vzduchu.cz.9ad09f98.js
+++ dist/odvlhcovac-vzduchu.cz.9ad09f98.js
@@ -186,7 +186,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./images/hero.jpg":[["hero.0f0b5fd4.jpg","images/hero.jpg"],"images/hero.jpg"],"_css_loader":"../../../../usr/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.css":[function(require,module,exports) {
+},{"./images/hero512.jpg":[["hero512.62c92c4c.jpg","images/hero512.jpg"],"images/hero512.jpg"],"./images/hero.jpg":[["hero.0f0b5fd4.jpg","images/hero.jpg"],"images/hero.jpg"],"_css_loader":"../../../../usr/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -218,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42087" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43731" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
